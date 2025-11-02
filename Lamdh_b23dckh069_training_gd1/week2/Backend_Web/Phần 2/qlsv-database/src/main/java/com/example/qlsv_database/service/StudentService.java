adbbfafcb654432bb25f4387248e164affd2e712
Lapisbee1413 <167759920+Lapisbee1413@users.noreply.github.com>
2025-11-02 11:13:23 +0700

    Add week2

---
+++ Lamdh_b23dckh069_training_gd1/week2/Backend_Web/Phần 2/qlsv-database/src/main/java/com/example/qlsv_database/service/StudentService.java
@@ -0,0 +1,42 @@
+package com.example.qlsv_database.service;
+
+import com.example.qlsv_database.model.Student;
+import com.example.qlsv_database.repository.StudentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+
+public class StudentService {
+    private final StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public List<Student> findAll() {
+        return studentRepository.findAll();
+    }
+
+    public Student findById(int id) {
+        return studentRepository.findById(id).orElseThrow(() -> new RuntimeException("Not found: " + id));
+    }
+
+    public Student addStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    public Student updateStudent(int id, Student sinhvien) {
+        Student std =  findById(id);
+        std.setName(sinhvien.getName());
+        std.setAge(sinhvien.getAge());
+        std.setMajor(sinhvien.getMajor());
+        return studentRepository.save(std);
+    }
+
+    public void deleteStudent(int id) {
+        Student std = findById(id);
+        studentRepository.delete(std);
+    }
+}
