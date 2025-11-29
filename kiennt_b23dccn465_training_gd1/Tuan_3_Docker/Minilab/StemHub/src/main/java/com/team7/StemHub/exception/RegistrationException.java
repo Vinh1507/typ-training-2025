1208bd6a632117a641e4701323cc76bab14e49ad
NguyenTuKien <KienNT.B23CN465@stu.ptit.edu.vn>
2025-11-29 18:59:26 +0700

    Docs

--- kiennt_b23dccn465_training_gd1/Tuan_3_Docker/Minilab/StemHub/src/main/java/com/team7/StemHub/exception/RegistrationException.java
+++
@@ -1,7 +0,0 @@
-package com.team7.StemHub.exception;
-
-public class RegistrationException extends RuntimeException {
-    public RegistrationException(String message) {
-        super(message);
-    }
-}
--- kiennt_b23dccn465_training_gd1/Tuan_3_Docker/Minilab/StemHub/src/main/java/com/team7/StemHub/exception/ServerException.java
+++
@@ -1,7 +0,0 @@
-package com.team7.StemHub.exception;
-
-public class ServerException extends RuntimeException {
-    public ServerException(String message) {
-        super(message);
-    }
-}
